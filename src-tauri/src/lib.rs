61d535aa17ef08da93ea3497c9828ee9a3146fe0
virusescu <virusescu@gmail.com>
2026-04-07 19:04:17 +0300

    feat: register tauri-plugin-updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_store::Builder::default().build())
+    .plugin(tauri_plugin_updater::Builder::new().build())
     .invoke_handler(tauri::generate_handler![
       oauth::wait_for_oauth_callback,
       oauth::get_available_port,
